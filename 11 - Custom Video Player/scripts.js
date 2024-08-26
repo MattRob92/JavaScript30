3ae854d164927f3f6a0459da4db26732e7e568fb
MattRob92 <164926430+MattRob92@users.noreply.github.com>
2024-08-26 12:23:16 +0100

    scrubbing
    Scrubbing function has been added

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -34,6 +34,12 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
+
 /* Hook up the Even Listerners*/
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -42,4 +48,10 @@ video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skiptButtons.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
